69a63525e77df2eddfe04e678a9c448cf7c02dd9
Subhajit1409 <subhajit14ghosh@gmail.com>
2024-06-01 21:29:28 +0530

    Added some express configurations and some utility classes/functions

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" })); //mostly from forms
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); //mostly from url
+app.use(express.static("public"));
+
+// cookie-parser is mostly used to access the user's browser cookies from our
+// server and can also set cookies, basically perform CRUD operation
+
+app.use(cookieParser());
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 // require('dotenv').config({path: './env'});
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+const port = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error : ", error);
+      throw error;
+    });
+    app.listen(port, () => {
+      console.log(`Server is running at port : ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed !!! ", err);
+  });
 
 /*
 import express from "express";
